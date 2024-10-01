e0a37352da590665ddd5ae2e430b64deec778ff2
housnizalam <109956808+housnizalam@users.noreply.github.com>
2024-10-01 11:14:02 +0200

    ss

--- lib/v7_inheretenz/v7_main.dart
+++ lib/v7_inheretenz/v7_main.dart
@@ -2,6 +2,13 @@ import 'package:oop_tutorial/v5_aggregation.dart/v5_fahrer.dart';
 import 'package:oop_tutorial/v7_inheretenz/v7_auto.dart';
 
 void main() {
-  final auto = AutoV7(baujahr: DateTime(2020), fahrer: Fahrer(vorname: 'Max', nachname: 'Mustermann', alter: 30));
-      auto.details();
+  final auto = AutoV7(
+    baujahr: DateTime(2020),
+    fahrer: Fahrer(
+      vorname: 'Max',
+      nachname: 'Mustermann',
+      alter: 30,
+    ),
+  );
+  auto.details();
 }
