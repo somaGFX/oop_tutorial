d887c09b7bd6976cf7ce9c86807a824e37707612
housnizalam <109956808+housnizalam@users.noreply.github.com>
2024-10-21 12:00:30 +0200

    v10 fertig

--- lib/v10_mixin/krieg_auto.dart
+++ lib/v10_mixin/krieg_auto.dart
@@ -2,7 +2,7 @@ import 'package:oop_tutorial/v10_mixin/mixin_maschinengewehr.dart';
 import 'package:oop_tutorial/v10_mixin/mixin_rakete.dart';
 import 'package:oop_tutorial/v10_mixin/v10_fahrzeug.dart';
 
-class KriegsMobil extends V10Fahrzeug with Rakete,Maschinengewehr {
+class KriegsMobil extends V10Fahrzeug with Rakete, Maschinengewehr {
   KriegsMobil({
     required super.baujahr,
     required super.fahrer,
@@ -17,3 +17,5 @@ class KriegsMobil extends V10Fahrzeug with Rakete,Maschinengewehr {
     print('KriegAuto fährt');
   }
 }
+
+
