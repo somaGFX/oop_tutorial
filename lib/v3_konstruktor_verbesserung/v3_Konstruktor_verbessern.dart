448f606a20da7626dc8a3fa8d8a414581cbd9538
Ulf Leis <ulf_leis@teilnehmer.cdmey.de>
2024-12-11 12:47:03 +0100

    new

--- lib/v3_konstruktor_verbesserung/v3_Konstruktor_verbessern.dart
+++ lib/v3_konstruktor_verbesserung/v3_Konstruktor_verbessern.dart
@@ -1,8 +1,7 @@
-
-
 void main() {
-  // mit named parameter Konstruktor muss die Reihenfolge der Parameter nicht beachtet werden
-  // Aber der Name des Parameters muss beim Bau des Objekts gegeben werden
+  // mit dem Named-Parameter-Konstruktor muss die Reihenfolge der Parameter
+  // nicht beachtet werden.
+  // Allerdings _muss_ der Name des Parameters beim Bau des Objekts angegeben werden.
   final auto = AutoV3(baujahr: DateTime(2020), marke: 'Mercedes');
   auto.marke = 'Mercedes';
   auto.reifenZahl = -5;
@@ -12,7 +11,7 @@ void main() {
 class AutoV3 {
   String material = 'metal';
   int reifenZahl = 4;
-  int insasseZahl;
+  int insassenZahl;
   DateTime baujahr;
   int alter;
   double reifenRadius;
@@ -21,22 +20,23 @@ class AutoV3 {
   String marke;
   // Named Parameter
   AutoV3({
-    // (This) Schlusselwort weist zur aktuellen Klassenojekt zu.
-    // die Parameter die mit required markiert sind, müssen beim Bau des Objekts gegeben werden
+    // (This) Schlüsselwort verweist zum aktuellen Klassenojekt.
+    // Die Parameter die mit _required_ markiert sind, müssen
+    // beim Bau des Objekts angegeben werden
     required this.baujahr,
     required this.marke,
-    // Wenn der Parameter standardwert hat oder nullable ist, darf nich required sein
+    // Wenn der Parameter einen Standardwert hat oder nullable ist,
+    // darf dieser nicht required sein
     this.reifenRadius = 30,
     this.reifenBreite = 20,
     this.alter = 24,
-    this.insasseZahl = 5,
+    this.insassenZahl = 5,
     this.fahrer,
   }) {
     print('AutoV3 created');
   }
 
   void fahren(int geschwindigkeit) {
-
     print('AutoV3 fährt');
   }
 
@@ -44,19 +44,19 @@ class AutoV3 {
     print('AutoV3 bremst');
   }
 
-  void liefern() {
-    print('AutoV3 5 Persone liefert');
+  void transportiern() {
+    print('AutoV3 5 Personen werden transportiert');
   }
 
   void details() {
     print('Material: $material');
-    print('InsasseZahl: $insasseZahl');
+    print('InsassenZahl: $insassenZahl');
     print('Baujahr: $baujahr');
     print('Alter: $alter');
-    print('Reifen Radius: $reifenRadius');
-    print('Reifen Breite: $reifenBreite');
+    print('Reifen-Radius: $reifenRadius');
+    print('Reifen-Breite: $reifenBreite');
     print('Fahrer: $fahrer');
     print('Marke: $marke');
-    print('Reifen Zahl: $reifenZahl');
+    print('Reifen-Anzahl: $reifenZahl');
   }
 }
